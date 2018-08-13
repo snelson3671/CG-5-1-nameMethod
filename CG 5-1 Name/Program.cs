f6cf2d7b7f369a90283504450aefdd5baf3942ff
Sandra <snelson3671@yahoo.com>
2018-08-13 16:50:36 -0500

    Add project files.

---
+++ CG 5-1 Name/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_5_1_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
