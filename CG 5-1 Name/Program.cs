b76133d3465baf4f9e4245bdb1feb730cd7971bb
Sandra <snelson3671@yahoo.com>
2018-08-16 19:18:44 -0500

    rewrite:  use a method to concatinate two strings into one.

--- CG 5-1 Name/Program.cs
+++ CG 5-1 Name/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace CG_5_1_MethodNames
 {
@@ -6,39 +7,21 @@ namespace CG_5_1_MethodNames
     {
         static void Main(string[] args)
         {
-            //Ask user to type their first and last name
             Console.WriteLine("Type your first name and press enter.  ");
             string firstName = Console.ReadLine();
             Console.WriteLine("Type your last name and press enter.   ");
             string lastName = Console.ReadLine();
 
-            //Call the method created below, putting the variable firstName
-            // then the variable lastName into it 
-            Console.Write("Your name spelled backwards is:    ");
-            ReverseString(firstName);
-            ReverseString(lastName);
-            
+            string fullName = NewString(firstName, lastName);
+            Console.WriteLine(fullName);
+
             Console.ReadLine();
         }
 
-        //Create a method called ReverseString allowing the caller 
-        //to pass in a strings called firstName and lastName
-        static void ReverseString(string fullName)
+        private static string NewString(string nameA, string nameB)
         {
-            //change the variable fullName to a character array and name it characters
-            char[] characters = fullName.ToCharArray();
-            //reverse the characters in the array
-            Array.Reverse(characters);
-
-            //write each character in the array then write a space
-            foreach (char item in characters)
-            {
-                Console.Write(item);
-            }
-
-                Console.Write(" ");
-            
-            
+            string fullName = ($"Hello {nameA} {nameB}");
+            return fullName;
         }
 
     }
